030020880c22e6dccf8ed4e5e6e9b8e829bb614c
IvanDancil <ivanbdancil@gmail.com>
2025-11-10 23:19:30 +0800

    Updated: latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/MRIS">
+      <BrowserRouter basename="/MRIS-frt">
         <Approutes />
          {/* Toast Container */}
          <ToastContainer 
